ad03fa542215bd6b5cc0735c86bc57c2538a7eca
Samuelyago07 <127311300+Samuelyago07@users.noreply.github.com>
2023-05-19 11:50:46 -0300

    Add files via upload

---
+++ aula.py
@@ -0,0 +1,34 @@
+print("Mensagem que eu quero", "TESTE", sep="")
+
+print('Mensagem 2')
+
+print('Fulano é meu "amigo"')
+
+print("Fulano \n é meu \"amigo\"")
+
+print(1+1)
+
+print("1 x 1=",1*1)
+
+print("10³ =",10**3)
+
+print("10³ =", pow (10,3))
+
+
+
+
+nome = str("A")
+x = int(1)
+x = 1+1
+y = True 
+
+D1 = 5.51
+D2 = "5"
+print(int(D1 + int(D2)))
+print(round(D1 + int(D2)))
+
+
+
+
+
+
